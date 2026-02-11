cc36d6df083e46d3455a601374d6d3b8493196ac
Christoph Hader <christoph.hader@gmail.com>
2026-02-11 08:40:57 -0700

    adding webapp hook

---
+++ apps/webapp/app.py
@@ -0,0 +1,15 @@
+# BSD-3-Clause License
+"""gasdyn - Inviscid compressible flow calculator."""
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="gasdyn",
+    layout="wide",
+)
+
+st.title("gasdyn")
+st.subheader("Inviscid Compressible Flow Calculator")
+
+st.info("**Coming Soon**")
+
